9954bfc5537710d05ea2e0269eaba1a3377a13ec
Chris Harrison <ctharrison1896@gmail.com>
2025-03-02 23:46:52 +0000

    fixing base 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ToppiTheThinker.github.io',
-  base: '/sparkhacks2025/',
+  base: '/sparkhacks2025',
   integrations: [react()]
 });
